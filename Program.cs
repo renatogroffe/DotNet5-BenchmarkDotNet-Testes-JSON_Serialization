b5d3293e5c0e6c56e46d2f1661b031a19b23e214
Renato Groffe <renato.groffe@yahoo.com.br>
2021-09-19 19:31:48 -0300

    .NET 5 + ConsoleApplication + BenchmarkDotNet + Newtonsoft + System.Text.Json
    .NET 5 + ConsoleApplication + BenchmarkDotNet + Newtonsoft + System.Text.Json

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Running;
+using TestesBenchmark.Tests;
+
+namespace TestesBenchmark
+{
+    class Program
+    {
+        static void Main()
+        {
+            BenchmarkRunner.Run<NewtonsoftVsSystemTextJson>();
+        }
+    }
+}
